16da6d020dfdc43a5e4c5faf9420980434816218
Ingrida1202 <danguoleingrida@gmail.com>
2023-05-07 15:23:42 +0300

    Pridetos reiksmes

---
+++ javascript2.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+
+    let x = -47;
+    x = -47;
+    console.log(x);
+
+    let y = 25 + x;
+    y = 25 + (x);
+    console.log(y);
+
+    let z = x + y;
+    z = x + y;
+    console.log(z);
+    console.log((-47) + (-22));
+    
+
+}
